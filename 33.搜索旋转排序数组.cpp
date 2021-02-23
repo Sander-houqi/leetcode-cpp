98a54365a25868d26ebfac03f337cd42f1087196
侯琦 <houqi1@staff.sina.com.cn>
2021-02-23 16:42:35 +0800

    二分查找

---
+++ 33.搜索旋转排序数组.cpp
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=33 lang=cpp
+ *
+ * [33] 搜索旋转排序数组
+ */
+#include<vector>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    // 题解参考81题
+    int search(vector<int>& nums, int target) {
+        
+        int n = nums.size(), l = 0 , r = n-1 ;
+        while (l<=r)
+        {
+            int mid = l + (r-l) /2;
+
+            if(nums[mid] == target){
+                return mid;
+            }else if (nums[mid]>=nums[l]) //左区间有序
+            {   
+                if(nums[l] <= target && target <= nums[mid]){
+                    r = mid - 1;
+                }else
+                {
+                    l = mid + 1;
+                }
+            }else
+            {
+                if(nums[mid]<= target && target <= nums[r]){
+                    l = mid + 1;
+                }else
+                {
+                    r = mid -1 ;
+                }
+            }
+        }
+        return -1;        
+    }
+};
+// @lc code=end
+
--- 81.搜索旋转排序数组-ii.cpp
+++ 81.搜索旋转排序数组-ii.cpp
@@ -11,16 +11,20 @@ using namespace std;
 // @lc code=start
 class Solution {
 public:
+    // 二分查找的前提是数组有序，所以此题要用二分查找必须在子区间缩小范围
     bool search(vector<int>& nums, int target) {
         int l= 0 , r = nums.size()-1,mid;
         while(l<=r){
             mid = l + (r-l)/2;
             if (nums[mid]==target) return true;
             
+            // 无法判断，则右移位
             if (nums[mid]==nums[l]){
                 ++l;
+
             //左区间增序
             }else if (nums[mid]>=nums[l]){
+                // 判断target的位置
                 if (target<=nums[mid] && target>=nums[l]){
                     r = mid -1 ;
                 }else
@@ -29,6 +33,7 @@ public:
                 }
             }else
             {   
+                //右区间增序
                 if (target>=nums[mid] && target<=nums[r]){
                     l = mid + 1;
                 }else
